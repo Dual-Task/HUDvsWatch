30a8433ff3add2e2accdc5a22bb8836c6f0fc554
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2018-10-29 16:12:07 -0400

    Add study subject form

--- mobile/src/main/java/edu/gatech/ic/hudvswatch/MainActivity.java
+++ mobile/src/main/java/edu/gatech/ic/hudvswatch/MainActivity.java
@@ -1,13 +1,58 @@
 package edu.gatech.ic.hudvswatch;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    List<String> studyConditions = new ArrayList<String>() {{
+        add("");
+        add("Visual Search");
+        add("HUD");
+        add("Watch");
+        add("Visual Search + HUD");
+        add("Visual Search + Watch");
+    }};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+
+        addConditionsToSpinner();
+    }
+
+    public void onStartButtonClick(View v) {
+        String subjectId = ((EditText) findViewById(R.id.subject_id_text)).getText().toString();
+        String condition = ((Spinner) findViewById(R.id.conditions_spinner)).getSelectedItem().toString();
+        Boolean isTraining = !((ToggleButton) findViewById(R.id.study_mode_toggle_button)).isChecked();
+
+        Log.d(TAG, String.format("Subject ID = %s, Condition = %s, Is Training = %s", subjectId, condition, isTraining.toString()));
+    }
+
+    public void addConditionsToSpinner() {
+        Spinner conditionsSpinner = findViewById(R.id.conditions_spinner);
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                studyConditions);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        conditionsSpinner.setAdapter(dataAdapter);
     }
 }
